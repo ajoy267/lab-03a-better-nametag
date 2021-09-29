0032877461b9a80c3cc42a8a84242ab24fa5db2b
ajoy267 <andrew.joy12@hotmail.com>
2021-09-29 13:45:30 -0700

    added 3 buttoms to change color of nametag

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const nameInput = document.getElementById('name');
 const button = document.getElementById('generate');
 const firstName = document.getElementById('first-name');
+const colorBlue = document.getElementById('blue');
+const colorOrange = document.getElementById('orange');
+const colorYellow = document.getElementById('yellow');
+let backgroundColor = document.getElementById('colors');
+let bottomColor = document.getElementById('color');
 
 button.addEventListener('click', ()=>{
     let newName = nameInput.value;
     firstName.textContent = newName;
+});
+
+colorBlue.addEventListener('click', ()=>{
+    backgroundColor.style.background = 'blue';
+    bottomColor.style.background = 'blue';
+});
+
+colorOrange.addEventListener('click', ()=>{
+    backgroundColor.style.background = 'orange';
+    bottomColor.style.background = 'orange';
+});
+
+colorYellow.addEventListener('click', ()=>{
+    backgroundColor.style.background = 'yellow';
+    bottomColor.style.background = 'yellow';
 });
